baf14067617bd2edc07afb0ab9134ac2ccd77a23
David V. Lu!! <davidvlu@gmail.com>
2023-09-20 21:03:36 -0400

    Handle more database formats (#5)

--- generate_protobuffers.bash
+++ generate_protobuffers.bash
@@ -1,3 +1,3 @@
-wget https://raw.githubusercontent.com/signalapp/Signal-Android/main/app/src/main/proto/Backups.proto
+wget https://raw.githubusercontent.com/signalapp/Signal-Android/main/app/src/main/protowire/Backups.proto
 protoc --python_out=. Backups.proto
 protoc --python_out=. Reactions.proto
